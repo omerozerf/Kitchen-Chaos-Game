909af83b27b4e5afb82c6286dd11a0bfa09639ef
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-02-13 01:41:42 +0300

    add move func

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -1,18 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed;
+    
+    
+    private void Update()
     {
+        Vector2 inputVector = new Vector2(0, 0);
         
-    }
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = +1;
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        inputVector = inputVector.normalized;
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * (Time.deltaTime * moveSpeed);
+        Debug.Log(inputVector);
         
     }
 }
