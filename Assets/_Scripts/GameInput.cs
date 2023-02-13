4606e52cff49572d97c644848f753adf561c71b9
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-02-14 01:05:57 +0300

    input system refactor

--- Assets/_Scripts/GameInput.cs
+++ Assets/_Scripts/GameInput.cs
@@ -9,33 +9,20 @@ public class GameInput : MonoBehaviour
     public static GameInput Instance;
 
 
+    private PlayerInputActions playerInputActions;
+    
     private void Awake()
     {
         Instance = this;
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
     }
 
 
     public Vector2 GetMovementVectorNormalized()
     {
-        Vector2 inputVector = new Vector2(0, 0);
+        Vector2 inputVector =playerInputActions.Player.Move.ReadValue<Vector2>();
         
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-
         inputVector = inputVector.normalized;
 
         return inputVector;
