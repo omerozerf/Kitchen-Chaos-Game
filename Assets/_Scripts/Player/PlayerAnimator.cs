7cbdf10213e9c4c478a7ee6e17cf52c8fb6abc86
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-02-15 11:36:38 +0300

    Update PlayerAnimator.cs

--- Assets/_Scripts/Player/PlayerAnimator.cs
+++ Assets/_Scripts/Player/PlayerAnimator.cs
@@ -7,17 +7,22 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
     private const string IS_WALKING = "isWalking";
+    private static readonly int IS_WALKING_HASH = Animator.StringToHash(IS_WALKING);
     
     
     private Animator animator;
 
+    
     private void Awake()
     {
         animator = GetComponent<Animator>();
     }
 
+    
     private void Update()
     {
-        animator.SetBool(IS_WALKING, Player.Instance.IsWalking());
+        // animator.SetBool(IS_WALKING, Player.Instance.IsWalking());
+        animator.SetBool(IS_WALKING_HASH, Player.Instance.IsWalking());
+        
     }
 }
