ed3f1e412527dc4a32bd2b8ebb85b9a80e24e434
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-02-16 21:58:33 +0300

    so

--- Assets/_Scripts/KitchenObjectSO.cs
+++ Assets/_Scripts/KitchenObjectSO.cs
@@ -2,7 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu()]
 public class KitchenObjectSO : ScriptableObject
 {
-    
+    public Transform prefab;
+    public Sprite sprite;
+    public string objectName;
 }
