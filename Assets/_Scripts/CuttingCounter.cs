fb094fe29c04346c9c21695e2895d59cc8fb9b53
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-02-17 20:59:13 +0300

    cutting counter

---
+++ Assets/_Scripts/CuttingCounter.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuttingCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (!HasKitchenObject())
+        {
+            // There is no KitchenObject here
+            
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+                
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // Player not carrying anything
+            }
+        }
+        else
+        {
+            // There is a KitchenObject here
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+            }
+            else
+            {
+                // Player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
+    }
+}
--- Assets/_Scripts/Player/Player.cs
+++ Assets/_Scripts/Player/Player.cs
@@ -69,7 +69,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
             
             // Attempt only X movement
             Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
-            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+            canMove = moveDir.x != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
                 playerRadius, moveDirX, moveDistance);
 
             if (canMove)
@@ -83,7 +83,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
                 
                 //Attempt only Z movement
                 Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
-                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
+                canMove = moveDir.z != 0 && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight,
                     playerRadius, moveDirZ, moveDistance);
 
                 if (canMove)
