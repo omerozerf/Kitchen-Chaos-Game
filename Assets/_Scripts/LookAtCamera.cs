6605ce06c7f865ed713b0c0643a32be2484fbe24
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-02-18 15:04:53 +0300

    Look At Camera - update

--- Assets/_Scripts/LookAtCamera.cs
+++ Assets/_Scripts/LookAtCamera.cs
@@ -8,7 +8,9 @@ public class LookAtCamera : MonoBehaviour
     private enum Mode
     {
         LookAt,
-        LookAtInverted
+        LookAtInverted,
+        CameraForward,
+        CameraForwardInverted
     }
 
 
@@ -26,6 +28,12 @@ public class LookAtCamera : MonoBehaviour
             case Mode.LookAtInverted:
                 Vector3 dirFromCamera = transform.position - Camera.main.transform.position;
                 transform.LookAt(transform.position + dirFromCamera);
+                break;
+            case Mode.CameraForward:
+                transform.forward = Camera.main.transform.forward;
+                break;
+            case Mode.CameraForwardInverted:
+                transform.forward = -Camera.main.transform.forward;
                 break;
         }
     }
