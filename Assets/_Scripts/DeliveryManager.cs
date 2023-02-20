9b748dd205ebf4b593167bf8608196997bac6353
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-02-21 00:20:10 +0300

    Delivery Manager

--- Assets/_Scripts/DeliveryManager.cs
+++ Assets/_Scripts/DeliveryManager.cs
@@ -6,6 +6,8 @@ using Random = UnityEngine.Random;
 
 public class DeliveryManager : MonoBehaviour
 {
+    public static DeliveryManager Instance { get; private set; }
+    
     [SerializeField] private RecipeListSO recipeListSO;
     
     private List<RecipeSO> waitingRecipeSOList;
@@ -16,6 +18,7 @@ public class DeliveryManager : MonoBehaviour
 
     private void Awake()
     {
+        Instance = this;
         waitingRecipeSOList = new List<RecipeSO>();
     }
 
@@ -33,5 +36,53 @@ public class DeliveryManager : MonoBehaviour
                 waitingRecipeSOList.Add(waitingRecipeSO);
             }
         }
+    }
+
+
+    public void DeliverRecipe(PlateKitchenObject plateKitchenObject)
+    {
+        for (int i = 0; i < waitingRecipeSOList.Count; i++)
+        {
+            RecipeSO waitingRecipeSO = waitingRecipeSOList[i];
+
+            if (waitingRecipeSO.kitchenObjectSOList.Count == plateKitchenObject.GetKitchenObjectSOList().Count)
+            {
+                // Has the same number of ingredients
+                bool plateContentsMatchesRecipe = true;
+                foreach (KitchenObjectSO recipeKitchenObjectSO in waitingRecipeSO.kitchenObjectSOList)
+                {
+                    // Cycling through all ingredients in the recipe
+                    bool ingredientFound = false;
+                    foreach (KitchenObjectSO plateKitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+                    {
+                        // Cycling through all ingredients in the Plate
+                        if (plateKitchenObjectSO == recipeKitchenObjectSO)
+                        {
+                            // Ingredient matches!
+                            ingredientFound = true;
+                            break;
+                        }
+                    }
+
+                    if (!ingredientFound)
+                    {
+                        // This Recipe ingredient was not found on the Plate
+                        plateContentsMatchesRecipe = false;
+                    }
+                }
+
+                if (plateContentsMatchesRecipe)
+                {
+                    // Player delivered the correct recipe!
+                    Debug.Log("Player delivered the correct recipe!");
+                    waitingRecipeSOList.RemoveAt(i);
+                    return;
+                }
+            }
+        }
+        
+        // No matches found!
+        // Player did not deliver a correct recipe
+        Debug.Log("Player did not deliver a correct recipe");
     }
 }
