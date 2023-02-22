7915d1d5271c0d76599d5ca89eed00155b1890f3
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-02-23 01:34:06 +0300

    7.51

---
+++ Assets/_Scripts/ScritableObjects/AudioClipRefsSO.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class AudioClipRefsSO : ScriptableObject
+{
+    public AudioClip stoveSizzle;
+    public AudioClip[] chop;
+    public AudioClip[] deliveryFail;
+    public AudioClip[] delirevySuccess;
+    public AudioClip[] footstep;
+    public AudioClip[] objectDrop;
+    public AudioClip[] objectPickup;
+    public AudioClip[] trash; 
+    public AudioClip[] warning;
+
+
+
+}
---
+++ Assets/_Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClipRefsSO audioClipRefsSO;
+    
+    
+
+    private void Start()
+    {
+        DeliveryManager.Instance.OnRecipeSuccess += DeliveryManagerOnOnRecipeSuccess;
+        DeliveryManager.Instance.OnRecipeFailed += DeliveryManagerOnOnRecipeFailed;
+    }
+
+    private void DeliveryManagerOnOnRecipeFailed(object sender, EventArgs e)
+    {
+        PlaySound(audioClipRefsSO.deliveryFail, Camera.main.transform.position);
+    }
+
+    private void DeliveryManagerOnOnRecipeSuccess(object sender, EventArgs e)
+    {
+        PlaySound(audioClipRefsSO.delirevySuccess, Camera.main.transform.position);
+    }
+
+    private void PlaySound(AudioClip[] audioClipArray, Vector3 position, float volume = 1f)
+    {
+        PlaySound(audioClipArray[Random.Range(0, audioClipArray.Length)], position, volume);
+    } 
+    
+    
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
+    {
+        AudioSource.PlayClipAtPoint(audioClip, position, volume);
+    } 
+}
