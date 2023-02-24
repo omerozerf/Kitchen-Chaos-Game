6f6f6ba6b44cf0e8b20003647bc7ea124a4babdf
omer faruk Ozer <ozeromerfaruk@gmail.com>
2023-02-24 15:19:50 +0300

    Options, Audio Levels

--- Assets/_Scripts/SoundManager.cs
+++ Assets/_Scripts/SoundManager.cs
@@ -7,14 +7,22 @@ using Random = UnityEngine.Random;
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager Instance { get; private set; }
+
+
+    private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
     
     
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
 
 
+    private float volume =1f;
+
+
     private void Awake()
     {
         Instance = this;
+
+        volume = PlayerPrefs.GetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, 1f);
     }
 
 
@@ -77,8 +85,26 @@ public class SoundManager : MonoBehaviour
     }
 
 
-    public void PlayFootstepsSound(Vector3 position, float volume)
+    public void PlayFootstepsSound(Vector3 position, float volumeMultiplier)
     {
-        PlaySound(audioClipRefsSO.footstep, position, volume);
+        PlaySound(audioClipRefsSO.footstep, position, volumeMultiplier * volume);
+    }
+
+
+    public void ChangeVolume()
+    {
+        volume += .1f;
+        if (volume > 1f)
+        {
+            volume = 0f;
+        }
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
+    }
+
+
+    public float GetVolume()
+    {
+        return volume;
     }
 }
